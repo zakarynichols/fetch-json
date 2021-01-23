8d5078d68f52c5a49525cbff47e4359eb6f4910f
zaknicholsdev <zaknicholsdev1993@gmail.com>
2021-01-22 21:50:30 -0500

    unmarshal json response and iterate slice

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -9,23 +10,36 @@ import (
 )
 
 type Todo struct {
-	UserId    string `json:"userId"`
-	Id        string `json:"id"`
-	Title     string `json:"title"`
-	Completed string `json:"completed"`
+	UserId int    `json:"userId"`
+	Id     int    `json:"id"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
 }
 
 func main() {
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
 	if err != nil {
 		fmt.Print(err.Error())
 		os.Exit(1)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-	// p, _ := json.Marshal(body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%v", string(body))
+	fmt.Printf("%v\n", string(body))
+
+	var td []Todo
+	err = json.Unmarshal(body, &td)
+	fmt.Printf("%v\n", td)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	for i, v := range td {
+		fmt.Printf("Index: %v\n", td[i])
+		fmt.Printf("UserId: %v\n", v.UserId)
+		fmt.Printf("Id: %v\n", v.Id)
+		fmt.Printf("Body: %v\n", v.Body)
+		fmt.Printf("Title: %v\n", v.Title)
+	}
 }
